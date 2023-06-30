e8affd0f8840598f240beccfb99593a7addcc2e8
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2023-06-30 21:01:32 +0530

    Update node-group.tf

--- node-group.tf
+++ node-group.tf
@@ -36,7 +36,7 @@ resource "aws_eks_node_group" "eks-dev-worker-node" {
   cluster_name    = aws_eks_cluster.eks-control-plane.name
   node_group_name = "${var.project}-dev-worker-node"
   node_role_arn   = aws_iam_role.worker-node-role.arn
-  subnet_ids      = [aws_subnet.public-subnet.id, aws_subnet.private-subnet.id]
+  subnet_ids      = [aws_subnet.public-subnet.id]
 
   scaling_config {
     desired_size = 1
