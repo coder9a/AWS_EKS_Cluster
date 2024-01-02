2f947a8ef8886856b293096d87c871545b424283
coder9a <rajsinghbalhara@gmail.com>
2024-01-02 20:15:04 +0530

    updted eks instance type

--- variables.tf
+++ variables.tf
@@ -46,7 +46,7 @@ variable "Disk_Size" {
 variable "EC2_Instance_Type" {
   type        = list(string)
   description = "Instance type that is created"
-  default = [ "t2.micro" ]
+  default = [ "t2.small" ]
 }
 
 variable "project" {
